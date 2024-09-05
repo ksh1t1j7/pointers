1848387a2a0bddb4e2954cfc871dad47cf722999
ksh1t1j7 <chess.comkshitij@gmail.com>
2024-09-05 22:33:29 +0530

    Create increment_int_pointers.cpp

---
+++ increment_int_pointers.cpp
@@ -0,0 +1,19 @@
+//Kshitij  23070123075
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=10;
+    int *ptr;
+    ptr=&a;
+    cout<<ptr<<endl;
+    ptr++;
+    cout<<ptr<<endl;
+    return 0;
+}
+
+/*
+0x7ffe5d391874
+0x7ffe5d391878
+*/
